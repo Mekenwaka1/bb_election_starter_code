2a07846df98216623f0900c8284ab3b72620a0f9
Osemeke Nwaka <o.ch.nwaka@gmail.com>
2019-09-08 14:21:56 -0400

    Created final assignment

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+  axios
+    .get("https://bb-election-api.herokuapp.com/")
+    .then(response => {
+      const {data} = response;
+      const ul = document.querySelector("#candidates");
+      ul.innerHTML = "";
+      data.candidates.forEach(candidate => {
+        let li = document.createElement("li");
+        li.id = "candidate-" + candidate.id;
+        li.textContent = `${candidate.name}: (${candidate.votes} votes)`;
+        ul.appendChild(li);
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
